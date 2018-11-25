87af5d14e379e0578fd8978e83bd8a897123b5d1
Ph4ra0h <Wesley.jonatha2047@gmail.com>
2018-11-25 10:07:43 -0300

    0.0.1

---
+++ include.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+  
+  int x = 1;
+
+    
+
+while(x = 1){
+
+
+puts("\n");
+puts("\n");
+puts("\n");
+puts("\n");
+puts("\t     IIIIIIIIIINNNNNNNN        NNNNNNNN        CCCCCCCCCCCCCLLLLLLLLLLL            UUUUUUUU     UUUUUUUUDDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEE"); usleep( 100000 ); 
+puts("\t     I::::::::IN:::::::N       N::::::N     CCC::::::::::::CL:::::::::L            U::::::U     U::::::UD::::::::::::DDD   E::::::::::::::::::::E"); usleep( 100000 ); 
+puts("\t     I::::::::IN::::::::N      N::::::N   CC:::::::::::::::CL:::::::::L            U::::::U     U::::::UD:::::::::::::::DD E::::::::::::::::::::E"); usleep( 100000 ); 
+puts("\t     II::::::IIN:::::::::N     N::::::N  C:::::CCCCCCCC::::CLL:::::::LL            UU:::::U     U:::::UUDDD:::::DDDDD:::::DEE::::::EEEEEEEEE::::E"); usleep( 100000 ); 
+puts("\t       I::::I  N::::::::::N    N::::::N C:::::C       CCCCCC  L:::::L               U:::::U     U:::::U   D:::::D    D:::::D E:::::E       EEEEEE"); usleep( 100000 ); 
+puts("\t       I::::I  N:::::::::::N   N::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE:::::E             "); usleep( 100000 ); 
+puts("\t       I::::I  N:::::::N::::N  N::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE::::::EEEEEEEEEE   "); usleep( 100000 ); 
+puts("\t       I::::I  N::::::N N::::N N::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE:::::::::::::::E   "); usleep( 100000 ); 
+puts("\t       I::::I  N::::::N  N::::N:::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE:::::::::::::::E   "); usleep( 100000 ); 
+puts("\t       I::::I  N::::::N   N:::::::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE::::::EEEEEEEEEE   "); usleep( 100000 ); 
+puts("\t       I::::I  N::::::N    N::::::::::NC:::::C                L:::::L               U:::::D     D:::::U   D:::::D     D:::::DE:::::E             "); usleep( 100000 ); 
+puts("\t       I::::I  N::::::N     N:::::::::N C:::::C       CCCCCC  L:::::L         LLLLLLU::::::U   U::::::U   D:::::D    D:::::D E:::::E       EEEEEE"); usleep( 100000 ); 
+puts("\t     II::::::IIN::::::N      N::::::::N  C:::::CCCCCCCC::::CLL:::::::LLLLLLLLL:::::LU:::::::UUU:::::::U DDD:::::DDDDD:::::DEE::::::EEEEEEEE:::::E"); usleep( 100000 ); 
+puts("\t     I::::::::IN::::::N       N:::::::N   CC:::::::::::::::CL::::::::::::::::::::::L UU:::::::::::::UU  D:::::::::::::::DD E::::::::::::::::::::E"); usleep( 100000 ); 
+puts("\t     I::::::::IN::::::N        N::::::N     CCC::::::::::::CL::::::::::::::::::::::L   UU:::::::::UU    D::::::::::::DDD   E::::::::::::::::::::E"); usleep( 100000 ); 
+puts("\t     IIIIIIIIIINNNNNNNN         NNNNNNN        CCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLLLLLLL     UUUUUUUUU      DDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEE"); usleep( 100000 );
+
+puts("\n"); 
+
+puts("\t                 ╔═╗┌─┐┌┐┌┌─┐┬ ┬┬ ┌┬┐┌─┐┬─┐┬┌─┐    ┌─┐  ┌┬┐┌─┐┌─┐┌─┐┌┐┌┬  ┬┌─┐┬ ┬  ┬┬┌┬┐┌─┐┌┐┌┌┬┐┌─┐  ┌┬┐┌─┐  ┌─┐┌─┐┌─┐┌┬┐┬ ┬┌─┐┬─┐┌─┐"); usleep( 100000 );
+puts("\t                 ║  │ ││││└─┐│ ││  │ │ │├┬┘│├─┤    ├┤    ││├┤ └─┐├┤ │││└┐┌┘│ ││ └┐┌┘││││├┤ │││ │ │ │   ││├┤   └─┐│ │├┤  │ │││├─┤├┬┘├┤ "); usleep( 100000 );
+puts("\t                 ╚═╝└─┘┘└┘└─┘└─┘┴─┘┴ └─┘┴└─┴┴ ┴    └─┘  ─┴┘└─┘└─┘└─┘┘└┘ └┘ └─┘┴─┘└┘ ┴┴ ┴└─┘┘└┘ ┴ └─┘  ─┴┘└─┘  └─┘└─┘└   ┴ └┴┘┴ ┴┴└─└─┘"); usleep( 100000 );
+
+                                           
+puts("");sleep(5);
+
+
+//system("cls"); //Windows
+system("clear"); //Linux
+
+}
+
+	return 0;
+}
